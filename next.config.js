2abcd87cecafa3b57a3844eca53c7afcee89dd3a
Sagnik Ghosh <sagnik@worqhat.com>
2023-06-16 16:12:17 +0530

    fix: Complete the Project
    Took 1 hour 34 minutes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+require('dotenv').config();
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    nextConfig,
+    env: {
+        WORQHAT_API_KEY: process.env.WORQHAT_API_KEY,
+        WORQHAT_ORG_KEY: process.env.WORQHAT_ORG_KEY,
+    }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,74 @@
-import { useEffect, useState, useRef } from 'react';
+import {useState} from 'react';
 import '../app/globals.css';
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import mypic from '../public/worqhatlogo.png';
 import Head from 'next/head';
+import endent from 'endent';
 
 function HomePage() {
     return (
         <div>
             <Head>
                 <title>Text to Code Generator - Home</title>
-                <meta name="description" content="Generate human-readable code explanations effortlessly with WorqHat AI's Code to Text Generator. Transform complex lines of code into clear and concise descriptions, enabling better collaboration and comprehension. Unlock the power of AI to enhance your programming experience today." />
+                <meta name="description"
+                      content="Generate human-readable code explanations effortlessly with WorqHat AI's Code to Text Generator. Transform complex lines of code into clear and concise descriptions, enabling better collaboration and comprehension. Unlock the power of AI to enhance your programming experience today."/>
             </Head>
 
-            <Home />
+            <Home/>
         </div>
     );
 }
 
 
-const Editor = dynamic(() => import('@monaco-editor/react'), { ssr: false });
+const Editor = dynamic(() => import('@monaco-editor/react'), {ssr: false});
+
+const createPrompt = (inputLanguage, inputCode) => {
+    return endent`
+    You are an expert programmer in all programming languages. Translate the natural language to "${inputLanguage}" code. Do not include \\\\.
+
+        Example translating from natural language to JavaScript:
+
+        Natural language:
+        Print the numbers 0 to 9.
+
+    JavaScript code:
+        for (let i = 0; i < 10; i++) {
+            console.log(i);
+        }
+
+    Natural language:
+        ${inputCode}
+
+        ${inputLanguage} code (no \\\\):
+    `;
+};
 
 async function fetchAIResponse(selectedLanguage, question) {
-  const API_ENDPOINT = 'https://api.worqhat.com/api/ai/content/v2';
-  const API_KEY = 'U2FsdGVkX187FPQxzgbmIVjXh3O1+xyor30KWVrIBMuFEqGv8NfzXPjE53e3Ju+T';
-  const ORG_KEY = 'U2FsdGVkX19lq3bhhF5TRouMiyL2HvEBD2V5j5nNl6dNL9JWPbsXW0rqlzssW8GieFki6oRVDKTb/z01Hc7m+Q==';
+    const API_ENDPOINT = 'https://api.worqhat.com/api/ai/content/v2';
 
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'x-api-key': API_KEY,
-      'x-org-key': ORG_KEY,
-    },
-    body: JSON.stringify({
-      question,
-      preserve_history: true,
-      history_object: {
-        'Selected Language': selectedLanguage,
-      },
-      randomness: 0.4,
-    }),
+    const API_KEY = process.env.WORQHAT_API_KEY;
+    const ORG_KEY = process.env.WORQHAT_ORG_KEY;
+
+    console.log(API_KEY);
+
+    const prompt = createPrompt(selectedLanguage, question);
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': API_KEY,
+            'x-org-key': ORG_KEY,
+        },
+        body: JSON.stringify({
+            question: prompt,
+            randomness: 0.4,
+        }),
   };
 
   const response = await fetch(API_ENDPOINT, requestOptions);
-  const data = await response.json();
-  return data;
-}
-
-function SelLanguage({ selectedLanguage }) {
-  const [response, setResponse] = useState('');
-  const editorRef = useRef(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      const responseData = await fetchAIResponse(selectedLanguage, '');
-      setResponse(responseData.content);
-      editorRef.current?.setValue(responseData.content);
-    }
-    fetchData();
-  }, [selectedLanguage]);
-
-  return (
-    <div>
-      <br />
-      <br></br>
-      {/* <h1>Generation Complete</h1> */}
-      <h2>Selected Language: {selectedLanguage}</h2>
-    </div>
-  );
+    return await response.json();
 }
 
 function Home() {
@@ -78,16 +78,22 @@ function Home() {
   const [loading, setLoading] = useState(false);
 
   const handleGenerateCodeClick = async () => {
-    const language = document.getElementById('language').value;
-    const textBoxValue = document.getElementById('textbox1').value;
+      const language = document.getElementById('language').value;
+      const questionText = document.getElementById('textbox1').value;
 
-    setSelectedLanguage(language);
-    setEnteredText(textBoxValue);
-    setLoading(true);
+      setSelectedLanguage(language);
+      setEnteredText(questionText);
+      setLoading(true);
 
-    const responseData = await fetchAIResponse(language, textBoxValue);
-    setGeneratedCode(responseData?.content || '');
-    setLoading(false);
+      let responseData = await fetchAIResponse(language, questionText);
+      if (responseData?.status == 'success') {
+          const responseCode = responseData?.content || '';
+          setGeneratedCode(`${responseCode}`);
+          setLoading(false);
+      } else {
+          setGeneratedCode('Sorry, we could not generate code for your question. Please try again.');
+          setLoading(false);
+      }
   };
 
   return (
@@ -128,38 +134,48 @@ function Home() {
             <option value="powershell">PowerShell</option>
         </select>
 
-      <button id="generateButton" onClick={handleGenerateCodeClick}>
-        {loading ? 'Generating...' : 'Generate Code'}
-      </button>
+            <div className="w-full text-center">
+                <button id="generateButton"
+                        className="mx-auto text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-xl px-5 py-2.5 text-center mr-2 mb-2"
+                        onClick={handleGenerateCodeClick}>
+                    {loading ? `Writing your Code ....` : 'Generate Code'}
+                </button>
+            </div>
 
-      {selectedLanguage && <SelLanguage selectedLanguage={selectedLanguage} />}
-      <div className="edit">
-        <Editor
-          height="400px"
-          language="javascript"
-          theme="vs-dark"
-          defaultValue="// Generated code will appear here"
-          defaultLanguage="javascript"
-          value={generatedCode}
-          options={{
-            readOnly: true,
-          }}
+            <div className="edit">
+                <Editor
+                    height="400px"
+                    language={selectedLanguage}
+                    theme="vs-dark"
+                    defaultValue="// Generated code will appear here"
+                    defaultLanguage="javascript"
+                    value={generatedCode}
+                    options={{
+                        readOnly: true,
+                        wordWrap: "wordWrapRow", // Enable word wrapping at a specific column
+                        wordWrapRow: 10,
+                    }}
         />
         <br />
       </div>
     </div>
-        <div className="text-center">
-          <h2  className="text-center mt-6">Powered By</h2>
-          <a href="https://worqhat.com">
-            <Image src={mypic} className="w-24 mx-auto" alt="WorqHat Logo" />
-          </a>
-        </div>
-        <div className="text-center mt-6 text-black hover:text-blue-700">
-          <a href="https://twitter.com/intent/tweet?text=Check%20out%20this%20awesome%20code%20Generator%20Tool%20using%20WorqHat%20AI!&url=https%3A%2F%2Fworqhat.com" className="inline-flex">
-            Share on Twitter
-            <svg className="ml-2 my-auto" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path fill="currentColor" d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/></svg>
-          </a>
-        </div>
+          <div className="text-center">
+              <h2 className="text-center mt-6">Powered By</h2>
+              <a href="https://worqhat.com">
+                  <Image src={mypic} className="w-24 mx-auto" alt="WorqHat Logo"/>
+              </a>
+          </div>
+          <div className="text-center my-6 text-black hover:text-blue-700 text-xl">
+              <a href="https://twitter.com/intent/tweet?text=Check%20out%20this%20awesome%20code%20Generator%20Tool%20using%20WorqHat%20AI!&url=https%3A%2F%2Fworqhat.com"
+                 className="inline-flex">
+                  Share on Twitter
+                  <svg className="ml-2 my-auto" xmlns="http://www.w3.org/2000/svg" height="1em"
+                       viewBox="0 0 512 512">
+                      <path fill="currentColor"
+                            d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/>
+                  </svg>
+              </a>
+          </div>
       </div>
   );
 }
